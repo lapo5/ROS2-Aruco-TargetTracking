6950f5ee1c9d570fe89d41baac65470d7c6bd29c
Marco Lapolla <marco.lapolla5@gmail.com>
2021-07-29 16:01:06 +0200

    fix

--- launch/tracking_and_transform_tree.launch.py
+++ launch/tracking_and_transform_tree.launch.py
@@ -12,12 +12,6 @@ def generate_launch_description():
         Node(
             package='camera_target_tracking',
             namespace='cam_target_tracking',
-            executable='ptu_to_robot_transform',
-            name='ptu_to_robot_transform'
-        ),
-        Node(
-            package='camera_target_tracking',
-            namespace='cam_target_tracking',
             executable='camera_to_ptu_base',
             name='camera_to_ptu_base'
         )
