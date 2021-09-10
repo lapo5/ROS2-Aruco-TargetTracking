5794ac97d5077ad3a02eaa27bddf52072963e9ff
roncapat <ronca.pat@gmail.com>
2021-09-10 12:00:03 +0200

    Fix

--- launch/target_tracking.py
+++ launch/target_tracking.py
@@ -5,33 +5,33 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='hal_allied_vision_camera',
-            namespace='rear_camera',
+            namespace='sensors',
             executable='av_node',
             name='hal_allied_vision_camera'
         ),
         Node(
             package='hal_flir_d46',
-            namespace='ptu',
+            namespace='sensors',
             executable='HALFlirD46',
             name='hal_flir_d46'
         ),
         Node(
             package='camera_target_tracking',
-            namespace='cam_target_tracking',
+            namespace='target_tracking',
             executable='aruco_detector',
             name='aruco_detector'
         ),
         Node(
             package='camera_target_tracking',
-            namespace='cam_target_tracking',
+            namespace='target_tracking',
             executable='camera_to_ptu_base',
             name='camera_to_ptu_base'
         ),
         Node(
             package='pasqua_tf',
             namespace='tf_tree',
-            executable='tf_tree',
-            name='pasqua_tf_tree',
+            executable='tf_tree_rear_camera',
+            name='tf_tree_rear_cam_pasqua',
             output='screen',
         ),
     ])
