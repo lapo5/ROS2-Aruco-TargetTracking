c971c9968581a684c38bbd5c1050ac6a196ee838
marcolapo5 <marco.lapolla5@gmail.com>
2021-09-10 11:58:31 +0200

    Fix namespaces

--- launch/target_tracking.py
+++ launch/target_tracking.py
@@ -4,6 +4,18 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
+            package='hal_allied_vision_camera',
+            namespace='rear_camera',
+            executable='av_node',
+            name='hal_allied_vision_camera'
+        ),
+        Node(
+            package='hal_flir_d46',
+            namespace='ptu',
+            executable='HALFlirD46',
+            name='hal_flir_d46'
+        ),
+        Node(
             package='camera_target_tracking',
             namespace='cam_target_tracking',
             executable='aruco_detector',
@@ -14,5 +26,12 @@ def generate_launch_description():
             namespace='cam_target_tracking',
             executable='camera_to_ptu_base',
             name='camera_to_ptu_base'
-        )
+        ),
+        Node(
+            package='pasqua_tf',
+            namespace='tf_tree',
+            executable='tf_tree',
+            name='pasqua_tf_tree',
+            output='screen',
+        ),
     ])
