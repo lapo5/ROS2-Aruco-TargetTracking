72743977af875125e24436174b473f70f3216e49
Antonino Bongiovanni <antoniobongio@gmail.com>
2021-10-05 11:11:06 +0200

    Added params.yaml per project, modifed launch file

--- launch/target_tracking.launch.py
+++ launch/target_tracking.launch.py
@@ -2,6 +2,7 @@ from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 import launch_ros.actions
 import os
+import sys
 import yaml
 from launch.substitutions import EnvironmentVariable
 import pathlib
@@ -12,6 +13,15 @@ from launch_ros.actions import Node
 from launch.actions.execute_process import ExecuteProcess
 
 def generate_launch_description():
+
+    params = os.path.join(get_package_share_directory("camera_target_tracking"), 'params', 'params_pasqualone.yaml')
+    
+    for arg in sys.argv:
+        if arg.startswith("project:="):
+            project = arg.split(":=")[1]
+            params = os.path.join(get_package_share_directory("camera_target_tracking"), 'params', 'params_' + project + '.yaml')
+
+
     return LaunchDescription([
         
         Node(
@@ -22,6 +32,6 @@ def generate_launch_description():
                     "stdout": "screen",
                     "stderr": "screen",
             },
-            parameters=[os.path.join(get_package_share_directory("camera_target_tracking"), 'params', 'params.yaml')],
+            parameters=[params],
         )
 ])
--- launch/target_tracking_stack.launch.py
+++ launch/target_tracking_stack.launch.py
@@ -2,6 +2,7 @@ from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 import launch_ros.actions
 import os
+import sys
 import yaml
 from launch.substitutions import EnvironmentVariable
 import pathlib
@@ -10,21 +11,31 @@ from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
 def generate_launch_description():
+
+    params_allied = os.path.join(get_package_share_directory("hal_allied_vision_camera"), 'params', 'params_pasqualone.yaml')
+    params_aruco = os.path.join(get_package_share_directory("camera_target_tracking"), 'params', 'params_pasqualone.yaml')
+    for arg in sys.argv:
+        if arg.startswith("project:="):
+            project = arg.split(":=")[1]
+            params_allied = os.path.join(get_package_share_directory("hal_allied_vision_camera"), 'params', 'params_' + project + '.yaml')
+            params_aruco = os.path.join(get_package_share_directory("camera_target_tracking"), 'params', 'params_' + project + '.yaml')
+
+
     return LaunchDescription([
         Node(
             package='hal_allied_vision_camera',
             executable='av_node',
-            name='hal_allied_vision_camera',
-            parameters=[os.path.join(get_package_share_directory("hal_allied_vision_camera"), 'params', 'params.yaml')],
+            name='av_node',
+            parameters=[params_allied],
         ),
         Node(
             package='camera_target_tracking',
             executable='aruco_detector',
-            name='aruco_detector',
+            name='aruco_pose_estimator',
             output={
                     "stdout": "screen",
                     "stderr": "screen",
             },
-            parameters=[os.path.join(get_package_share_directory("camera_target_tracking"), 'params', 'params.yaml')],
+            parameters=[params_aruco],
         ),
 ])
