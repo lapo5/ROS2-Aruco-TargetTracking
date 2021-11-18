edddc62245de71471b340fff7932b039b5eed944
marcolapo5 <marco.lapolla5@gmail.com>
2021-11-18 18:35:13 +0100

    Update

--- launch/rviz.launch.py
+++ launch/rviz.launch.py
@@ -27,7 +27,7 @@ def generate_launch_description():
         Node(
             package='rviz2',
             executable='rviz2',
-            name='rviz2',
+            name='rviz2_for_target_tracking',
             arguments=['-d', rviz_config_path]
         ),
 
